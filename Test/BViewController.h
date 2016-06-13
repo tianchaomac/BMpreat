02c916484e21a48b4b6a26425f6e29abc7c16fc5
John <John.163.com>
2016-06-13 17:07:07 +0800

    新建一个类

---
+++ Test/BViewController.h
@@ -0,0 +1,13 @@
+//
+//  BViewController.h
+//  Test
+//
+//  Created by mac on 16/6/13.
+//  Copyright © 2016年 IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BViewController : UIViewController
+
+@end
